e7d779fad4f36b2d1c9c6843eaf6e60464326209
paul <pauliix.santamaria@gmail.com>
2020-12-23 00:03:33 +0100

    docker working with db

--- app/siteLeoClimb/settings.py
+++ app/siteLeoClimb/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'debug_toolbar',
+]   
 
 
 
@@ -62,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'siteLeoClimb.urls'
@@ -96,7 +99,7 @@ DATABASES = {
         'NAME': 'leoclimb',
         'USER': 'paul',
         'PASSWORD': 'paultapedes7a',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
@@ -149,14 +152,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "./appLeoClimb/static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#STATICFILES_DIR = (
-#    os.path.join(BASE_DIR, 'appLeoClimb/static')
-#    )
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-#LOGIN_REDIRECT_URL = '/'
+INTERNAL_IPS = ['127.0.0.1']
